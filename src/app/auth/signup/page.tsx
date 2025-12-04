476a9497492c653c5ace4acc4756485c8611b035
Jahsfreedman Moffat <xcoder2442@gmail.com>
2025-12-04 00:24:13 +0000

    Try fixing this error: `Build Error: Parsing ecmascript source code fail

--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -97,7 +97,7 @@ export default function SignupPage() {
                 <FormField
                     control={form.control}
                     name="password"
-                    render={({ field })_ => (
+                    render={({ field }) => (
                     <FormItem>
                         <FormLabel>Password</FormLabel>
                         <FormControl>
